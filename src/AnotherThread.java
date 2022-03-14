656356bc8d0fba1e3b6fdc84c221ea46654057ac
mkpou <mkpoutoe@hotmail.com>
2022-03-14 21:35:19 +1100

    Runnable and Threads

--- src/AnotherThread.java
+++ src/AnotherThread.java
@@ -14,18 +14,23 @@ public class AnotherThread extends Thread {
 
 	@Override
 	public void run() {
-		System.out.println(//ThreadColor.ANSI_BLUE + 
-		"Hello from another thread" 
-	//	+ currentThread().getName()
+		System.out.println(ThreadColor.ANSI_BLUE + 
+	//	"Hello from another thread"  //this is the original display statement in this method for Release 1.0 dev-Beta on develop branch
+	"Hello from "	+ currentThread().getName(); //This statement is used to test the difference between calling
+												//the run() or start() method on a thread instance.
 	);
 		
-	//	try {
-	//		Thread.sleep(5000);
-	//	}catch(InterruptedException e) {
-	//		System.out.println("Another thread woke me up");
-	//		return;
-	//	}
+	//Modification to display thread code performing other tasks apart from creating and starting threads
+		try {
+			Thread.sleep(3000); //sets the thread to sleep for 3 seconds i.e. measurement is in milliseconds in sleep() method
+								//so 3000 milliseconds = 3 seconds. Sleep() method also has overloaded method that accepts a second
+								//parameter for nanoseconds
+		}catch(InterruptedException e) {
+			System.out.println(ANSI_BLUE + "Another thread woke me up");//To be executed if another thread wakes up this thread
+			return;
+		}
 		
-	//	System.out.println("Three seconds have passed and I'm awake");
+	//	The method below executes after 3 seconds to demonstrate thread performing the sleep task if the InterruptedException is not thrown
+	System.out.println("Three seconds have passed and I'm awake");
 	}
 }
